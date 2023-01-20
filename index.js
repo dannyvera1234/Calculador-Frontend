bbd70ee218b738faa40308a4638b59539b9019e3
danny <manuelcarrrillopenuela51@gmail.com>
2023-01-19 21:57:53 -0500

    calculadora diseño

---
+++ index.js
@@ -0,0 +1,11 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumero = document.querySelectorAll('numero');
+const botonesOperado = document.querySelectorAll('operador');
+
+const display =new Display (displayValorAnterior, displayValorActual);
+
+botonesNumero.forEach(boton =>{
+    boton.addEventListener('click', () =>
+        display.agregarNumero(boton.innerHTML));
+});
